309226d31a1708a1f670639fc4dd7ebfc0931ae1
Maanav Rao <58347499+NPUZenith@users.noreply.github.com>
2020-11-11 23:20:15 -0500

    First Lecture

---
+++ src/Robot.java
@@ -0,0 +1,13 @@
+public class Robot
+{
+    public static void main(String[] args)
+    {
+        Robot robot = new Robot();
+        robot.speak();
+    }
+
+    void speak()
+    {
+        System.out.println("Hi!");
+    }
+}
