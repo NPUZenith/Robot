00b3f3e5672a5b4fc45d6578ed54f4fc5b57e030
Maanav Rao <58347499+NPUZenith@users.noreply.github.com>
2020-11-13 11:26:57 -0500

    Part 4

--- src/Robot.java
+++ src/Robot.java
@@ -2,11 +2,17 @@ public class Robot
 {
     String name;
 
+    Robot (String name)
+    {
+        this.name = name;
+    }
+
     public static void main(String[] args)
     {
-        Robot r = new Robot();
-        r.name = "Boi";
+        Robot r = new Robot("Boi");
         r.speak("Hello! My name is ");
+        Robot s = new Robot("Bob");
+        s.speak("Wassup! My name is ");
     }
 
     void speak(String msg)
